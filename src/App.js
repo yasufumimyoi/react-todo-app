ce72cd52d4b08aeef0a7f6fc26912c302556393c
Yasufumi Myoi <yasufumimyoi@yasufuminoMacBook-Air.local>
2020-07-04 23:43:09 +0900

    review-2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { Page1, Page2, Page3 } from "./memo5";
+import Memo6 from "./memo6";
 
-function App() {
-  return (
-    <div>
-      <p>This is from App</p>
-      <Page1 />
-      <Page2 />
-      <Page3 />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Max",
+      city: "Tokyo",
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Memo6 />
+      </div>
+    );
+  }
 }
 
 export default App;
