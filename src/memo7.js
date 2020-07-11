f9abbe9453145267197794a57dd75bfc636a40ff
Yasufumi Myoi <yasufumimyoi@yasufuminoMacBook-Air.local>
2020-07-11 22:27:35 +0900

    react-review5

--- src/memo7.js
+++ src/memo7.js
@@ -1,16 +1,19 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 
 const memo7 = (props) => {
   return (
     <div>
       <li key={props.index}>{props.item}</li>
-      <button
+      <Button
+        variant="contained"
+        color="primary"
         onClick={(e) => {
           props.handleRemove(props.item);
         }}
       >
         Remove
-      </button>
+      </Button>
     </div>
   );
 };
