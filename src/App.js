526c40e82f7b35fc75ffd144bcefb63ecc7b9c23
Yasufumi Myoi <yasufumimyoi@yasufuminoMacBook-Air.local>
2020-07-19 19:28:23 +0900

    react-review10

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
 import React from "react";
 import Memo8 from "./memo8";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+
+const Header = () => (
+  <header>
+    <h3>Todo App</h3>
+    <NavLink to="/" activeClassName="selected" exact={true}>
+      Dashboard
+    </NavLink>
+    <NavLink to="create" activeClassName="selected">
+      Create List
+    </NavLink>
+  </header>
+);
+
+const HomePage = () => (
+  <div>
+    <p>Write down things to do</p>
+  </div>
+);
+
+const CreatePage = () => (
+  <div>
+    <p>Put your tasks down the below</p>
+  </div>
+);
+
+const NotFoundPage = () => (
+  <div>
+    <p>Not Found</p>
+    <Link to="/">Go Home</Link>
+  </div>
+);
 
 class App extends React.Component {
   constructor(props) {
@@ -11,9 +43,17 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div>
-        <Memo8 />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/create" component={CreatePage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+          <Memo8 />
+        </div>
+      </BrowserRouter>
     );
   }
 }
